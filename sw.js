5f4459d1c270d00791fe5785663ad7b712d55cee
akiakaneeee <u352097h@ecs.osaka-u.ac.jp>
2025-07-11 16:04:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('timer-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './manifest.json',
+        './icon.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
 
